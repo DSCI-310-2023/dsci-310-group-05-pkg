5ed20ce65e934eb460320b039a5381d0b9e8d66c
DaliaAh <ahmaddalia708@gmail.com>
2023-04-09 14:50:16 -0700

    added more tests for create_recipe

--- tests/testthat/test-create_recipe.R
+++ tests/testthat/test-create_recipe.R
@@ -1,30 +1,32 @@
-# Test case 1
+# Test case 1: Test that create_recipe returns a recipe object
 test_that("create_recipe returns a recipe object", {
-  expect_s3_class(test_recipe, expected_recipe_class)
+  result <- create_recipe(df, response_var)
+  expect_s3_class(result, expected_recipe_class)
 })
 
-# Test case 2
-test_that("create_recipe throws an error if response variable not in dataset", {
-  expect_error(
-    create_recipe(df, not_response_var),
-    "response_var must be a column in data."
-  )
-})
-
-# Test case 3
-test_that("create_recipe throws an error when data is missing", {
+# Test case 2: Test that create_recipe throws an error if data is null
+test_that("create_recipe throws an error if data is null", {
   expect_error(create_recipe(NULL, response_var), "data must be provided.")
 })
 
-# Test case 4
-test_that("create_recipe throws an error when data is missing", {
+# Test case 3: Test that create_recipe throws an error if response_var is null
+test_that("create_recipe throws an error if response_var is null", {
   expect_error(create_recipe(df, NULL), "response_var must be provided.")
 })
 
-# Test case 5
-test_that("create_recipe throws an error when data is missing", {
+# Test case 4: Test that create_recipe throws an error if response_var is not a
+# character string
+test_that("create_recipe throws an error if response_var is not a character
+          string", {
   expect_error(
     create_recipe(df, 123),
     "response_var must be a character string."
   )
+})
+
+# Test case 5: Test that create_recipe throws an error if response_var is not
+# a column in data
+test_that("create_recipe throws an error if response_var is not a column in
+          data", {
+  expect_error(create_recipe(df, not_response_var))
 })
